3b08417a426630ef8d7602e8cdbaf64e4f9fac2b
Liz Burke <liz.burke.chi@gmail.com>
2021-01-13 20:09:27 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,14 @@
+#def find_element_index(array, value_to_find)
+#  count = 0
+#  found_value_index = nil
+#  while count < array.length do
+#    if array[count] == value_to_find
+#    return count
+#  end
+#    count += 1
+#  end
+#end
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.index(value_to_find)
 end
